833aa8eb0282cb2f4f5395f626eb56f7cdad3b7e
MufaddalTaherAli-hub <mufaddaltaherali615@gmail.com>
2025-01-06 15:41:08 +0530

    Add files via upload

---
+++ Home_page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+
+from prediction_page import show_predict_page
+show_predict_page()
+
+
---
+++ prediction_page.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import pickle
+
+
+regressor = pickle.load(open('new_forest_regressor.pkl', 'rb'))
+
+le = LabelEncoder()
+le = pickle.load(open('le.pkl', 'rb'))
+
+def show_predict_page():
+    st.title('Taxi Fare Prediction')
+
+    st.image('Taxi image.png', width=200)
+
+    ## Create user imput
+
+    Distance_in_km = st.number_input("Distance in Km", min_value=1, max_value=None, value=None)
+    Traffic_condition = st.selectbox("Traffic Condition", ['Low', 'High', 'Medium'])
+    Weather = st.selectbox("Weather", ['Clear', 'Rain', 'Snow'])
+
+    # mapping
+    Traffic_condition_mapping = {'Low':1, 'High':0, 'Medium':2}
+    Weather_mapping = {'Clear':0, 'Rain':1, 'Snow':2}
+
+    # convert to numeric
+    Traffic_condition_numeric = Traffic_condition_mapping[Traffic_condition]
+    Weather_numeric = Weather_mapping[Weather]
+
+
+    ## create prediction button
+    ok = st.button("Submit")
+    if ok:  
+        # create np array
+        x = np.array([[Distance_in_km, Traffic_condition_numeric, Weather_numeric]])
+
+        # Predict using the loaded model
+    
+        prediction = regressor.predict(x)
+        st.subheader(f"Taxi fare : ${np.round(prediction, 2)}")
+
+        
